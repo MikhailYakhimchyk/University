0a373ef30442af1ced885b67fab1fa9f2ee7cc90
MikhailYakhimchyk <miha200341@gmail.com>
2022-04-04 21:45:12 +0200

    first commit

---
+++ Rekursja/main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int Factorial(int N)
+{
+	if (N == 0)
+		return 0;
+	if (N == 1)
+		return 1;
+	return N * Factorial(N - 1);
+}
+
+void main()
+{
+	int Num;
+	cout << "Please input number: ";
+	cin >> Num;
+	cout << "!" << Num << " = ";
+	cout << Factorial(Num) << endl;
+}
